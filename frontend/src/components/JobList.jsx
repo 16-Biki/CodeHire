c5e1669549025192007c17550fbe0f47a48cf92c
16-Biki <mahadarbikram@gmail.com>
2026-03-22 13:46:41 +0530

    Final Submit

--- frontend/src/components/JobList.jsx
+++ frontend/src/components/JobList.jsx
@@ -4,18 +4,27 @@ import JobCard from "./JobCard";
 
 function JobList() {
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
   const user = JSON.parse(localStorage.getItem("user"));
 
   const fetchJobs = async () => {
-    const res = await API.get("/jobs");
+    try {
+      setLoading(true);
 
-    if (user?.role === "admin") {
-      const filtered = res.data.filter(
-        (job) => job.companyId === user.companyId,
-      );
-      setJobs(filtered);
-    } else {
-      setJobs(res.data);
+      const res = await API.get("/jobs");
+
+      if (user?.role === "admin") {
+        const filtered = res.data.filter(
+          (job) => job.companyId === user.companyId,
+        );
+        setJobs(filtered);
+      } else {
+        setJobs(res.data);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -25,11 +34,13 @@ function JobList() {
 
   return (
     <div className="job-list">
-      {jobs.length === 0 && <p>No jobs available</p>}
+      {loading && <p>Loading jobs...</p>}
 
-      {jobs.map((job) => (
-        <JobCard key={job._id} job={job} />
-      ))}
+      {!loading && jobs.length === 0 && <p>No jobs available</p>}
+
+      {!loading &&
+        jobs.length > 0 &&
+        jobs.map((job) => <JobCard key={job._id} job={job} />)}
     </div>
   );
 }
